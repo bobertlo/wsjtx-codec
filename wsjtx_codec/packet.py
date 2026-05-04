5acbffcfb6e33b7c0bcb61247a4558a8012b0497
Robert Lowry <bobertlo@gmail.com>
2026-05-04 09:20:09 -0500

    add decode_packet, modify decode_heartbeat to include header

--- wsjtx_codec/packet.py
+++ wsjtx_codec/packet.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from wsjtx_codec.qdatastream import QDataStreamReader
 
 MAGIC = 0xADBCCBDA
+HEARTBEAT_TYPE = 0
 
 
 @dataclass
@@ -23,17 +24,28 @@ def decode_header(r: QDataStreamReader) -> Header:
 
 
 @dataclass
-class Heartbeat:
+class HeartbeatPacket:
+    schema: int
+    type: int
     id: str | None
     max_schema: int
     version: str | None
     revision: str | None
 
 
-def decode_heartbeat(r: QDataStreamReader) -> Heartbeat:
-    return Heartbeat(
+def decode_heartbeat(header: Header, r: QDataStreamReader) -> HeartbeatPacket:
+    return HeartbeatPacket(
+        schema=header.schema,
+        type=header.type,
         id=r.read_utf8(),
         max_schema=r.read_u32(),
         version=r.read_utf8(),
         revision=r.read_utf8(),
     )
+
+
+def decode_packet(r: QDataStreamReader) -> HeartbeatPacket:
+    header = decode_header(r)
+    if header.type == HEARTBEAT_TYPE:
+        return decode_heartbeat(header, r)
+    raise ValueError(f"unknown packet type: {header.type}")
